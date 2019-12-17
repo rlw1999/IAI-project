28669d1d0737d6319b07059d7c6dc40bfeea9cdc
pkuccr <chengchaoran@pku.edu.cn>
2019-12-17 17:06:39 +0800

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ccr/Desktop/IAI-project/catkin_ws/src
+# Build directory: /home/ccr/Desktop/IAI-project/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(autobin_description)
+subdirs(autobin_gazebo)
+subdirs(autobin_control)
---
+++ catkin_ws/build/autobin_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ccr/Desktop/IAI-project/catkin_ws/src/autobin_description
+# Build directory: /home/ccr/Desktop/IAI-project/catkin_ws/build/autobin_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
