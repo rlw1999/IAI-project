98c0aeeafee027a118f7b9ea20c8f31e1360507d
rlw1999 <13581236976@163.com>
2019-12-17 01:34:55 -0800

    clean

--- catkin_ws/build/autobin_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ccr/Desktop/IAI-project/catkin_ws/src/autobin_description
-# Build directory: /home/ccr/Desktop/IAI-project/catkin_ws/build/autobin_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
