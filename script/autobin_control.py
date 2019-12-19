6ff9da9a782a7c8de3ca2f61d097d7ef18d64842
guoyork <49684611+guoyork@users.noreply.github.com>
2019-12-19 17:34:58 +0800

    Add files via upload

--- script/autobin_control.py
+++ script/autobin_control.py
@@ -8,8 +8,8 @@ from pygazebo.msg import images_stamped_pb2, contacts_pb2, vector2d_pb2, vector3
 
 def cal_torq(*input):
     torqueRange = 1000
-    return random.uniform(-torqueRange, torqueRange), \
-           random.uniform(-torqueRange, torqueRange)
+    torque = random.uniform(-torqueRange, torqueRange)
+    return torque,-torque
 
 
 def update_loss(loss):
@@ -23,14 +23,29 @@ async def main_loop():
 
     vel = vector3d_pb2.Vector3d(x=0, y=0, z=0)
     pos = vector3d_pb2.Vector3d(x=0, y=0, z=0)
-
+    pos_x = 0.0
+    pos_y = 0.0
+    flag = 0
     def onImage(data):
         imgs = images_stamped_pb2.ImagesStamped.FromString(data).image
         # print('Received {1} image! height: {0.height}, width: {0.width}'.format(imgs[0], len(imgs)))
-
+        '''
         torq = cal_torq(imgs, vel)
         torq_msg = vector2d_pb2.Vector2d(x=torq[0], y=torq[1])
         asyncio.wait(torq_pub.publish(torq_msg))
+        '''
+	
+    def onBall(data):
+        nonlocal vel
+        vel = vector3d_pb2.Vector3d.FromString(data)
+        g = 9.81
+        time = math.sqrt(8.0/g)
+        nonlocal pos_x
+        pos_x = time*vel.x
+        nonlocal pos_y
+        pos_y = time*vel.y
+        #print('guess pos:%.3f, %.3f'%(pos_x,pos_y))
+        
 
     def onContact(data):
         contact = contacts_pb2.Contacts.FromString(data).contact
@@ -40,6 +55,8 @@ async def main_loop():
         loss = 0
         if contact[0].collision1[0] == 'a' or contact[0].collision2[0] == 'a':
             # hit the bin, the collision is actually `autobin::bin::collision`
+            hit_pos = contact[0].position[0]
+            print('hit pos:%.3f, %.3f'%(hit_pos.x,hit_pos.y))
             print('Autobin successfully catches the ball!')
         else:
             # miss the ball
@@ -48,16 +65,29 @@ async def main_loop():
             dy = pos.y - hit_pos.y
             dz = pos.z - hit_pos.z
             loss = math.sqrt(dx * dx + dy * dy + dz * dz)
+            print('hit pos:%.3f, %.3f'%(hit_pos.x,hit_pos.y))
             print('Autobin misses the ball! The distance is {:.3f}'.format(loss))
         update_loss(loss)
 
     def onVelocity(data):
         nonlocal vel
         vel = vector3d_pb2.Vector3d.FromString(data)
-
     def onPosition(data):
         nonlocal pos
+        nonlocal pos_y
+        nonlocal pos_x
         pos = vector3d_pb2.Vector3d.FromString(data)
+        tmp = (pos_x-pos.x)*(pos_x-pos.x) + (pos_y-pos.y)*(pos_y-pos.y)
+        vec_x = (pos_x-pos.x)/math.sqrt(tmp)
+        vec_y = (pos_y-pos.y)/math.sqrt(tmp)
+        if abs(vec_x-math.cos(pos.z)) < 0.01 and abs(vec_y-math.sin(pos.z)) < 0.01:
+            torq_msg = vector2d_pb2.Vector2d(x=100, y=100)
+            asyncio.wait(torq_pub.publish(torq_msg))
+            #print("get the position")
+        else:
+            #print("%.3f, %.3f, %.3f, %.3f"%(vec_x, math.cos(pos.z), vec_y, math.sin(pos.z)))
+            torq_msg = vector2d_pb2.Vector2d(x=100, y=-100)
+            asyncio.wait(torq_pub.publish(torq_msg))
 
     camera_sub = manager.subscribe('/gazebo/default/stereo_camera/link/camera/images',
                                    'gazebo.msgs.ImagesStamped', onImage)
@@ -67,6 +97,8 @@ async def main_loop():
                                 'gazebo.msgs.Vector3d', onVelocity)
     pos_sub = manager.subscribe('/gazebo/default/autobin/position',
                                 'gazebo.msgs.Vector3d', onPosition)
+    ball_sub = manager.subscribe('/gazebo/default/ball/velocity',
+                                'gazebo.msgs.Vector3d', onBall)
 
     while True:
         await asyncio.sleep(1.0)
